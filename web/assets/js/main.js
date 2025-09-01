91d8f72d3bb590846caa9e39a3dfb8fae1ce63ee
Muhammad Talha <mtalha0018@gmail.com>
2025-09-01 13:59:56 +0500

    JS Basic

---
+++ web/assets/js/main.js
@@ -0,0 +1,16 @@
+// **** DATA TYPES ****
+// number: 1212321, float: 2.3, dobule: 300000000000000
+// string: "This is some common text" 
+// array: myarray = ["Shoes", "T-Shirts"]
+// myarray[1]
+
+// oject: user = { address: "abc", name: "Asad" }
+// user.name
+// boolean: true, false
+
+// typeof: tells what is data type
+
+var a = 20;
+var b = 10;
+var c = a + b + 3 + (a*b + ab - 3 - b);
+console.log(c);
