03afb7be920dc0a7d81ae2be652e49ab58278e94
Muhammad Talha <mtalha0018@gmail.com>
2025-09-03 13:07:19 +0500

    js if else

---
+++ web/assets/js/ifelse.js
@@ -0,0 +1,21 @@
+
+function print() {
+  var a = "This is my data";
+
+  if (a != "") {
+    // when requirement will be true
+    console.log("Variable has value");
+  } else {
+    // when requirement will be false
+    console.log("Variable does not have value");
+  }
+
+  var x = 14;
+  var y = 6;
+
+  if (x < y) {
+    console.log("x is greater then y");
+  } else {
+    console.log("y is greater then x");
+  }
+}
--- web/assets/js/insertItem.js
+++ web/assets/js/insertItem.js
@@ -1,6 +1,14 @@
 function insertItem(){
     var newItem = document.getElementById("new-item").value;
+    var output = document.getElementById("result");
+
+    output.innerHTML += "<option>" + newItem + "</option>";
+}
+
+function showSelected(){
+    var val = document.getElementById("result").value;
     var output = document.getElementById("output");
 
-    output.innerHTML += "<li>" + newItem + "</li>";
+    output.innerText = val;
+
 }
