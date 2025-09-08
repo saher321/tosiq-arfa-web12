b05e86fe4883d556e414c7bf222a5de9911966df
Muhammad Talha <mtalha0018@gmail.com>
2025-09-08 13:22:51 +0500

    for loop

---
+++ web/assets/js/forloop.js
@@ -0,0 +1,11 @@
+// variable declare
+// variable initialize
+// condition
+// increament
+
+// 2 * 1 = 2
+
+for (let i = 1; i <= 10; i++) {
+    console.log("2" + " x " + i + " = " + (i*2));
+}
+console.log("loop completed")
--- web/assets/js/switch-assignment.js
+++ web/assets/js/switch-assignment.js
@@ -1,9 +1,30 @@
 
 function printValue(){
     let a = 10;
-    let b = 5;
+    let b = 6;
 
     let operator = document.getElementById("operator").value;
+    let output = document.getElementById("output");
+    // console.log(operator);
 
-    console.log(operator);
+    switch(operator){
+        case "+":
+            output.innerText = a+b;
+            break;
+        case "-":
+            output.innerText = a-b;
+            break;
+        case "/":
+            output.innerText = a/b;
+            break;
+        case "*":
+            output.innerText = a*b;
+            break;
+        case "%":
+            output.innerText = a%b;
+            break;
+        default: 
+            output.innerText = "Please choose operator";
+
+    }
 }
