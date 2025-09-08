66ecff5b2a8623e6e617eeb90807696da19bee30
Muhammad Talha <mtalha0018@gmail.com>
2025-09-08 13:42:25 +0500

    for loop array

--- web/assets/js/forloop.js
+++ web/assets/js/forloop.js
@@ -4,8 +4,17 @@
 // increament
 
 // 2 * 1 = 2
+let fruits = ["Apple", "Guawa", "Grapes", "Orange"];
+// console.log(fruits[0]);
+let list = document.getElementById("list");
 
-for (let i = 1; i <= 10; i++) {
-    console.log("2" + " x " + i + " = " + (i*2));
+for (let i = 0; i < fruits.length; i++) {
+    // console.log(fruits[i]);
+    list.innerHTML += "<li>" + fruits[i] + "</li>"
 }
-console.log("loop completed")
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log("2" + " x " + i + " = " + (i*2));
+// }
+// console.log("loop completed")
