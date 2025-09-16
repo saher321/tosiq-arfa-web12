e93603d914708c1cb835775decd6a1cb6d8eb0f4
Muhammad Talha <mtalha0018@gmail.com>
2025-09-16 13:45:53 +0500

    todo

--- web/assets/js/filterdata.js
+++ web/assets/js/filterdata.js
@@ -1,5 +1,27 @@
+const users = [
+    {id: 1, firstName: "John", lastName: "Samith", email: "john@gmail.com"},
+    {id: 2, firstName: "Sam", lastName: "Jonathan", email: "sam@gmail.com"},
+    {id: 3, firstName: "Jane", lastName: "Alexendar", email: "jane@gmail.com"},
+    {id: 4, firstName: "Adil", lastName: "Murfy", email: "Adil@gmail.com"},
+    {id: 5, firstName: "Usman", lastName: "Asad", email: "usman@gmail.com"}, 
+];
+
 $(document).ready(function (){
 
+    showUsers();
+    function showUsers(){
+        users.forEach((user, i) => {
+            let data = `
+            <tr> 
+                <td> ${i+1} </td> 
+                <td> ${user.firstName} </td> 
+                <td> ${user.lastName} </td> 
+                <td> ${user.email} </td> 
+            </tr>
+            `;
+            $("#myTable").append(data);
+        })
+    }
     $("#myInput").on("input", function (){
         // console.log($(this).val());
         let value = $(this).val();
@@ -7,6 +29,4 @@ $(document).ready(function (){
             $(this).toggle($(this).text().indexOf(value) > -1)
         })
     })
-
-
 })
