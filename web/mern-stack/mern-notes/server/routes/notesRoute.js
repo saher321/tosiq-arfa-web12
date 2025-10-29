5947ee9860f10004af5734ec313328b644b387aa
Muhammad Talha <mtalha0018@gmail.com>
2025-10-29 12:57:55 +0500

    AuthMiddleware

--- web/mern-stack/mern-notes/server/routes/notesRoute.js
+++ web/mern-stack/mern-notes/server/routes/notesRoute.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import { createNotes, deleteNotes, detailNote, getAllNotes, updateNote } from '../controllers/notesController.js';
+import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const notesRoute = express.Router();
 
+// public routes
 notesRoute.get('/notes', getAllNotes)
+
+// protected routes
+notesRoute.use(authMiddleware);
 notesRoute.post('/notes/create', createNotes)
 notesRoute.get('/notes/:id', detailNote)
 notesRoute.delete('/notes/delete/:id', deleteNotes)
